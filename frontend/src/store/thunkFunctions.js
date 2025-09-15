a6d48a776aa4368735dc0db47ad6cde847c07666
billihazero <billihazero@gmail.com>
2025-09-15 16:22:27 +0900

    feat: [frontend] 로그인 성공 시 Authorization 헤더에 Bearer 토큰 추가

--- frontend/src/store/thunkFunctions.js
+++ frontend/src/store/thunkFunctions.js
@@ -28,3 +28,15 @@ export const loginUser = createAsyncThunk(
     }
   }
 );
+
+export const authUser = createAsyncThunk(
+  "user/authUser",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axiosInstance.get(`/users/auth`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
