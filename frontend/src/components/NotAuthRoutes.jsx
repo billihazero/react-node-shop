c9700ef142c619fb6db5c97f3ec5862d434e3852
billihazero <billihazero@gmail.com>
2025-09-15 17:24:28 +0900

    feat: [frontend] 인증상태에 따른 라우팅 분기 처리
    - ProtectedRoutes를 통해 로그인 사용자만 접근 가능한 페이지 구현
    - NotAuthRoutes 를 통해 비로그인 사용자 전용 경로 분리

--- frontend/src/components/NotAuthRoutes.jsx
+++ frontend/src/components/NotAuthRoutes.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
-const NotAuthRoutes = () => {
-  return <div>NotAuthRoutes</div>;
+//로그인하지 않았다면 Outlet= 로그인, 회원가입으로
+const NotAuthRoutes = ({ isAuth }) => {
+  return isAuth ? <Navigate to={"/"} /> : <Outlet />;
 };
 
 export default NotAuthRoutes;
