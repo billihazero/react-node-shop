070f19580394fd52f22e23d68d260182dfab9a62
billihazero <billihazero@gmail.com>
2025-09-15 17:32:04 +0900

    feat: axiosInstance 추가 및 회원가입 기능 생성

--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "./../../../node_modules/react-redux/src/hooks/useDispatch";
+import { registerUser } from "../../store/thunkFunctions";
 const RegisterPage = () => {
   const {
     register,
@@ -8,7 +10,18 @@ const RegisterPage = () => {
     reset,
   } = useForm({ mode: "onChange" });
 
+  const dispatch = useDispatch();
+
   const onSubmit = ({ email, password, name }) => {
+    const body = {
+      email,
+      password,
+      name,
+      image: `https://via.placeholder.com/600x400?text=no+user+image`,
+    };
+
+    dispatch(registerUser(body));
+
     reset();
   };
 
